07622da8a26f35618eb29c2e8c141293128aeec4
amon1te <alenshakeyev@gmail.com>
2023-02-26 21:38:11 +0600

    many
    changes

--- main.js
+++ main.js
@@ -3,3 +3,22 @@ hamburger.onclick = function() {
     navBar = document.querySelector(".nav-bar");
     navBar.classList.toggle("active")
 }
+
+var myText = document.getElementById("my-Text");
+var result =  document.getElementById("result");
+var limit = 60;
+result.textContent = 0 + "/" + limit;
+myText.addEventListener("input", function() {
+    var textLength = myText.value.length;
+    result.textContent = textLength + "/" +  limit;
+
+    if(textLength > limit) {
+        myText.style.borderColor = "#ff2851";
+        result.style.color = "#ff2851";
+    }
+
+    else {
+        myText.style.borderColor = "#b2b2b2";
+        result.style.color = "#737373";
+    }
+});
