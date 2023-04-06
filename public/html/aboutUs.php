1170205c2f2636a25aa4cc5ccf17d784e332aa49
sprinterz5 <63397324+sprinterz5@users.noreply.github.com>
2023-04-07 01:27:07 +0600

    Update aboutUs.php

--- public/html/aboutUs.php
+++ public/html/aboutUs.php
@@ -8,6 +8,78 @@
     <h1>Кто мы такие?</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet deleniti vero praesentium. Numquam in tenetur consectetur pariatur tempora vero atque nemo eligendi laborum quam nulla voluptates, maiores, amet commodi ullam nesciunt, beatae eveniet consequuntur fugiat facilis libero voluptas. 
   </div>
+  <div class="about-us-team">
+    <div class="team-title">
+      Наша команда
+    </div>
+    <div class="team-subtitle">
+      Наша команда - это люди, которые отдали душу и тело нашему проекту, опытные специалисты в данной сфере.
+    </div>
+    <div class="team-cards">
+      <div class="team-card">
+        <img class="team-card-img" src="/public/Images/team-card.png">
+        <div class="team-card-title">
+          Манарбай <br>Аружан
+        </div>
+        <div class="team-card-subtitle">
+          Leader
+        </div>
+      </div>
+      <div class="team-card">
+        <img class="team-card-img" src="/public/Images/team-card-1.png">
+        <div class="team-card-title">
+          Сабитова <br>Никара
+        </div>
+        <div class="team-card-subtitle">
+          Educational manager
+        </div>
+      </div>
+
+      <div class="team-card">
+        <img class="team-card-img" src="/public/Images/team-card-2.png">
+        <div class="team-card-title">
+          Усейн <br>Зарема
+        </div>
+        <div class="team-card-subtitle">
+          SMM
+        </div>
+      </div>
+
+      <div class="team-card">
+        <img class="team-card-img" src="/public/Images/team-card-3.png">
+        <div class="team-card-title">
+          Литвинова <br>Есения
+        </div>
+        <div class="team-card-subtitle">
+          Educational manager
+        </div>
+      </div>
+
+      <div class="team-card">
+        <img class="team-card-img" src="/public/Images/team-card-4.png">
+        <div class="team-card-title">
+          Машрапова <br>Дильназ
+        </div>
+        <div class="team-card-subtitle">
+          Designer
+        </div>
+      </div>
+      <div class="team-card">
+        <img class="team-card-img" src="/public/Images/team-card-5.png">
+        <div class="team-card-title">
+          Нурабаева <br>Даяна
+        </div>
+        <div class="team-card-subtitle">
+          SMM
+        </div>
+      </div>
+      
+      
+      
+      
+      
+    </div>
+  </div>
   <div class="team">
     <h1>Наша команда</h1>
     <div class="image"></div>
